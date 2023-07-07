644bd6a9bef0d7d8b8b44a94fbe876ee94b6611f
Phyllis Nabangi <nabangiphyllis7@gmail.com>
2023-07-07 18:28:19 +0200

    modified the tranlate.py file

--- translate.py
+++ translate.py
@@ -1,12 +1,17 @@
 import requests
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 def translate_text(text: str, source_lang: str, target_lang: str) -> str:
     # API endpoint URL
     url = "https://sunbird-ai-api-5bq6okiwgq-ew.a.run.app"
     
-    # access token
-    access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJJbnRlcm5zaGlwcyIsImV4cCI6NDg0MTQ4NzEyMn0.-j3rdudJ9pXEm3-456LLiDPun5SwIm5sw-RoNvgDwfk"
-    
+    # Access the access token
+    access_token = os.getenv("token")
+
     headers = {
         "Authorization": f"Bearer {access_token}",
         "Content-Type": "application/json"
@@ -42,20 +47,27 @@ def main():
     If the source language is English, prompt the user to choose the target language.
     If the source language is not English, set the target language to English by default.
     """
-    source_language = input("Please choose the source language (English, Luganda, Runyankole, Ateso, Lugbara, or Acholi): ")
+    while True: 
+        source_language = input("Please choose the source language (English, Luganda, Runyankole, Ateso, Lugbara, or Acholi): ")
 
-    if source_language.lower() == "english":
-        target_language = input("Please choose the target language (Luganda, Runyankole, Ateso, Lugbara, or Acholi): ")
-    else:
-        target_language = "english"
+        if source_language.lower() == "english":
+            target_language = input("Please choose the target language (Luganda, Runyankole, Ateso, Lugbara, or Acholi): ")
+        else:
+            target_language = "english"
 
-    # Ask the user for the text to be translated
-    text = input("Enter the text to translate from the source language chosen: ")
+        # Ask the user for the text to be translated
+        text = input("Enter the text to translate from the source language chosen: ")
 
-    # Call the translate_text function to perform the translation
-    translated_text = translate_text(text, source_language.lower(), target_language.lower())
-    
-    print("Translated Text: ", translated_text)
+        # Call the translate_text function to perform the translation
+        translated_text = translate_text(text, source_language.lower(), target_language.lower())
+        
+        print("Translated Text: ", translated_text)
+
+        continue_input = input("Do you want to continue:")
+        if continue_input.lower() == "yes":
+            continue
+        else:
+            break
 
 
 if __name__ == "__main__":
